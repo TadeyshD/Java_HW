3a63f20331ca1b9557b4f0f52254a9b79b63f8e4
TadeyshD <86590703+TadeyshD@users.noreply.github.com>
2022-01-02 15:40:00 +0300

    Empty

---
+++ Java_home_work/src/Java_Home_Work/HW_8.java
@@ -0,0 +1,10 @@
+package Java_Home_Work;
+
+public class HW_8 {
+    public static void main(String[] args) {
+        hello_world("Hello java");
+    }
+    public static void hello_world(String Java){
+        System.out.println(Java);
+    }
+}
